7980cdfa50fa62c8622ce2e0976edac5ae688884
Hack Reactor Students <Jonsadka@gmail.com>
2014-08-01 22:11:28 -0700

    created d3 game with collision detection

--- watchout.js
+++ watchout.js
@@ -1,8 +1,26 @@
 // start slingin' some d3 here.
 //
 // Our game stage:
-var width = 960;
-var height = 500;
+var width = window.innerWidth;
+var height = window.innerHeight;
+var stats = {
+  highScore: 0,
+  currentScore: 0,
+  collisions: 0
+};
+var pause = false;
+var scaleEnemy = d3.scale.linear()
+                        .domain([0, 1280])
+                        .range([0, 8]);
+
+var scaleEnemyCount = d3.scale.linear()
+                        .domain([0, 1280])
+                        .range([10, 35]);
+
+var scalePlayer = d3.scale.linear()
+                          .domain([0, 1280])
+                          .range([5, 15]);
+
 
 // generates a set amount of enemies with unique locations
 var enemyPos = function(numEnemies){
@@ -12,11 +30,20 @@ var enemyPos = function(numEnemies){
   }
   return allEnemies;
 };
-
 // generates a random position
 var randomPos = function( size ){
   return Math.floor( Math.random() * size );
 };
+// create a function to handle drag events
+var dragMove = function(d){
+  d3.select(this)
+  .attr('cx', d3.event.x)
+  .attr('cy', d3.event.y);
+};
+// create drag behavior
+var drag = d3.behavior.drag()
+             .on('drag', dragMove);
+
 
 // CONTAINER creation
 var svg = d3.select('.stage')
@@ -25,9 +52,23 @@ var svg = d3.select('.stage')
             .attr('height', height)
             .style('background', '#333');
 
+// SCOREBOARD creation
+svg.selectAll('scoreboard')
+   .data(['High Score: ' + stats.highScore, 'Current Score: ' + stats.currentScore, 'Collisions: ' + stats.collisions])
+   .enter()
+   .append('text')
+   .text(function(d){ return d; } )
+   .attr('class', 'scoreboard')
+   .attr('x', 20)
+   .attr('y', function(d, i) {
+      return 30 + i*18;
+   })
+   .attr('font-size', '15px')
+   .attr('fill', '#fff');
+
 // ENEMY creation
-svg.selectAll('circle')
-  .data(enemyPos(5))
+svg.selectAll('.enemy')
+  .data( enemyPos(scaleEnemyCount(window.innerWidth) ) )
   .enter()
   .append('circle')
   .attr("class", "enemy")
@@ -37,26 +78,21 @@ svg.selectAll('circle')
   .attr('cy', function(d) {
     return d[1];
   })
-  .attr('r', function(d) {
-    return Math.sqrt(height - d[1]);
-  })
-  .attr('fill', 'red')
-  .attr('stroke-width', '3')
-  .attr('stroke', 'blue');
+  .attr('r', function(){ return scaleEnemy(window.outerWidth) + Math.floor(Math.random()*15)} )
+  .attr('fill', 'red');
 
 // PLAYER creation
-svg.selectAll('rect')
+svg.selectAll('player')
   .data([[]])
   .enter()
-  .append('rect')
+  .append('circle')
   .attr("class", "player")
-  .attr('width', 35 )
-  .attr('height', 15 )
-  .attr('x', width/2 )
-  .attr('y', height/2 )
+  .attr('cx', width/2 )
+  .attr('cy', height/2 )
+  .attr('r', scalePlayer(window.outerWidth))
   .attr('fill', 'yellow')
-  .attr('stroke-width', '3')
-  .attr('stroke', 'yellow');
+  .call(drag);
+
 
 var moveEnemy = function() {
   d3.selectAll('.enemy').each(function(){
@@ -68,8 +104,45 @@ var moveEnemy = function() {
 };
 
 var checkClash = function() {
+  // get position of player and compare to EVERY enemy on page
+  var playerPosX = svg.select('.player').attr('cx');
+  var playerPosY = svg.select('.player').attr('cy');
+  var playerSize = svg.select('.player').attr('r');
 
+  // loop through each enemy and check if outer edge collides with player
+  d3.selectAll('.enemy').each(function(){
+    var enemyPosX = d3.select(this).attr('cx');
+    var enemyPosY = d3.select(this).attr('cy');
+    var enemySize = d3.select(this).attr('r');
+    var distanceBetween =  Math.sqrt( Math.pow((playerPosX - enemyPosX),2) + Math.pow((playerPosY - enemyPosY),2) );
+    var collideDistance = +playerSize + +enemySize;
+
+    // if collides, add to collisions, save high score if applies and reset current score
+    if ( distanceBetween < collideDistance ){
+      stats.collisions++;
+      pause = true;
+      if(stats.currentScore > stats.highScore) {
+        stats.highScore = stats.currentScore;
+      }
+      stats.currentScore = 0;
+      setTimeout( function(){ pause = false; }, 500);
+    }
+
+  });
 };
 
+var updateScore = function() {
+  d3.selectAll('.scoreboard')
+      .data(['High Score: ' + stats.highScore, 'Current Score: ' + stats.currentScore, 'Collisions: ' + stats.collisions])
+     .text(function(d){ return d; } );
+};
+
+
+setInterval( function(){ updateScore(); }, 10);
+setInterval( function(){ stats.currentScore += 1; }, 10);
 setInterval( function(){ return moveEnemy(); }, 1000);
-setInterval( function(){ return checkClash(); }, 100);
+var enemyCheck = setInterval( function(){
+  if(!pause) {
+    return checkClash();
+  }
+}, 5);
